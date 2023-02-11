9fbf049b6dce16b5f88e1d4164b7b6517c994fa5
diditwbw <diditwbw@gmail.com>
2023-02-11 14:53:13 +0700

    test

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use std::collections::btree_set::Difference;
+
+fn main() {
+    // Integer
+
+    let a: i32 = 92_22; //Desimal
+    let b: i32 = 0xff; //hexa
+    let c: i32 = 0o77; //octal
+    let d: i32 = 0b1111_0000; //Binary
+    let e: u8 = b'A'; //Byte
+
+    let f: u8 = 256;
+
+    //Floating Number
+    let f: f64 = 2.0;
+    let g: f32 = 3.0;
+
+    //Addition
+    let sum: i32 = 5 + 10;
+
+    //substraction
+    let difference: f64 = 95.5 - 4.3;
+
+    //multiplication
+    let product: i32 = 4 * 30;
+    //division
+    let quotient: f64 = 56.7 / 32.2;
+
+    //remainder
+    let remainder: i32 = 43 % 5;
+
+    //Boolean
+    let t: bool = true;
+
+    let f: bool = false;
+
+    //character
+    let c: char = 'z';
+    let z: char = 'Z';
+    let hear_eyed_cat: char = 'd';
+
+}
